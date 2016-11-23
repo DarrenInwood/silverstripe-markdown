b01ded69b695f8894b83fb2e5c0cfe50d9720ce2
Mitchell Bennett <mitchell.bennett@solnet.co.nz>
2016-11-23 15:00:58 +1300

    Removed CloundinaryUpload and HtmlEditorField_Toolbar class substitute from _config.php as these should be opt in.

--- _config.php
+++ _config.php
@@ -1,9 +1,3 @@
 <?php
 
 define('MARKDOWN_MODULE_BASE', basename(dirname(__FILE__)));
-
-if(ClassInfo::exists('CloudinaryFileField')){
-    MarkdownEditorField::add_extension('MarkdownCloudinaryUpload');
-}
-
-Object::useCustomClass('HtmlEditorField_Toolbar', 'MarkdownEditorField_Toolbar', true);
